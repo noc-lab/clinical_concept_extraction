c4061c27e7555edf4e1b9ff3b4507948822aefc1
omar sayed <omar.sayed.mostafa>
2020-05-19 01:50:28 +0200

    added build_display_entity() function to return entity dictionary that conatains the start and the end of each label in given text

--- clinical_concept_extraction/utils.py
+++ clinical_concept_extraction/utils.py
@@ -50,46 +50,16 @@ def get_ents (listOfdic_entities , text):
     return listOfent_label
 
 
-
-
-def build_display_elements(tokens, annotations, spans):
-    # convert the annotations to the format used in displacy
-    all_ann = []
-
-    for sent_id, sent_info in enumerate(tokens):
-        sent_length = len(tokens[sent_id])
-
-        last_ann = 'O'
-        last_start = None
-        last_end = None
-        for token_id in range(sent_length):
-            this_ann = annotations[sent_id][token_id]
-
-            # separated cases:
-            if this_ann != last_ann:
-                if last_ann != 'O':
-                    # write last item
-                    new_ent = {}
-                    new_ent['start'] = last_start
-                    new_ent['end'] = last_end
-                    new_ent['label'] = last_ann[2:]
-                    all_ann.append(new_ent)
-
-                # record this instance
-                last_ann = 'O' if this_ann == 'O' else 'I' + this_ann[1:]
-                last_start = spans[sent_id][token_id][0]
-                last_end = spans[sent_id][token_id][1]
-
-            else:
-                last_ann = this_ann
-                last_end = spans[sent_id][token_id][1]
-
-        if last_ann != 'O':
+def build_display_elements(annotations):
+    display_entities = []
+    text = ''
+    for i, annotation in enumerate(annotations):
+        token, span, label = annotation
+        text += token+' '
+        if label !='O':
             new_ent = {}
-            new_ent['start'] = last_start
-            new_ent['end'] = last_end
-            new_ent['label'] = last_ann[2:]
-            all_ann.append(new_ent)
-
-    return all_ann
-
+            new_ent['start'] = span[0]
+            new_ent['end'] = span[1]+1
+            new_ent['label'] = label[2:]
+            display_entities.append(new_ent)
+    return display_entities, text
